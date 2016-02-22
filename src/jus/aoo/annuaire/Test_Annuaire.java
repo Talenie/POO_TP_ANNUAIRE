3c433fa9f9894cba9930fcb8f170a0b96fb23d41
Gilles B <bonhoure.gilles@gmail.com>
2016-02-22 16:05:40 +0100

    Parties 5, 6 et 7
    La 7 Bug, je sais pas trop pourquoi. Surement que mon compareTo dans Personne est pas correct (on ne peut pas ajouter deux personnes de nom identiques, on dirait).

--- src/jus/aoo/annuaire/Test_Annuaire.java
+++ src/jus/aoo/annuaire/Test_Annuaire.java
@@ -6,13 +6,25 @@ public class Test_Annuaire {
 		Annuaire A;
 		A = new Annuaire();
 		Personne p;
-		p = new Personne("Gilles","Bonhoure","Mr");
+		p = new Personne("Bonhoure","Gilles","Mr");
 		String n;
+		
 		n = "0650403020";
 		
 		A.loadEntryFromFile("annuaire.txt");
 		
 		A.addEntry(p, n);
 		System.out.println(A.toString());
+		
+		System.out.println("------------------\n");
+		System.out.println("Noms commencant par B\n");
+		System.out.println(A.toString('B'));
+		
+		System.out.println("------------------\n");
+		System.out.println("Ajout de nouvelles entrées depuis un fichier.\n");
+		A.addEntryFromFile("ajouter.txt");
+		System.out.println(A.toString());
+		
+	
 	}
 }
