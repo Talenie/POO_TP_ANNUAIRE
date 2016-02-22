72e13eb2d7b42cbef19ace7c08c8c546c2bc18b9
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2016-02-22 13:35:23 +0100

    lecture OK. MAIS ajout dans le .txt des 0 en début de numéro

--- src/jus/aoo/annuaire/Annuaire.java
+++ src/jus/aoo/annuaire/Annuaire.java
@@ -1,9 +1,6 @@
 package jus.aoo.annuaire;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
